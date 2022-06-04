6e72b3df53ae7cf08cc93e853ece288388bf7ab2
Darsh Patel <47074897+DarshRPatel@users.noreply.github.com>
2022-06-05 01:15:20 +0530

    Committing files

---
+++ CodeChef/C++14/IRSTXOR/43546704.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<long long> vl;
+typedef pair<long long, long long> pl;
+typedef vector<vector<long long>> matrix;
+
+#define F                         first
+#define S                         second
+#define PB                         push_back
+#define MP                         make_pair
+#define all(c)                     c.begin(), c.end()
+#define f(i,a,b)                 for(ll i=a; i<b; i++)
+#define rep(i,n)                 f(i,0,n)
+#define fd(i, b, a)                for(ll i=b; i>=a; i--)
+#define repr(i,n)                fd(i, n-1, 0)
+#define tr(c,i)                 for(typeof(c).begin() i = c.begin(); i != c.end(); i++)
+#define present(c,x)             (c.find(x) != c.end())    //for set and map
+#define cpresent(c,x)             (find(all(c),x) != c.end())    //for vectors
+#define ps(num, places)            fixed<<setprecision(places)<<num //use as cout<<ps(x, y)<<"\n";
+#define sz(x)                     (ll)(x).size()
+
+const ld epsilon = 1e-9;
+const ll MOD = 1e9+7;
+
+bool comp(ll a, ll b) {
+    return (a > b);
+}
+
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while(b>0) {
+        if(b&1) res *= a;
+        a = a*a;
+        b >>= 1;
+    }
+    return res;
+}
+
+void runcase() {
+    ll c;
+    cin >> c;
+    matrix a(2, vl(31, 0));
+    ll a_sum = 0, b_sum = 0;
+    repr(i, 31) {
+        if(c&(1<<i)) {
+            if(b_sum<a_sum) {
+                a[0][i] = 0;
+                a[1][i] = 1;
+                b_sum += (1<<i);
+            }else {
+                a[0][i] = 1;
+                a[1][i] = 0;
+                a_sum += (1<<i); 
+            }
+        }else if(c>=(1<<i)) a[0][i] = 1, a[1][i] = 1;
+    }
+    // rep(i, 31) cout<<a[0][i]<<" ";
+    // cout<<"\n";
+    // rep(i, 31) cout<<a[1][i]<<" ";
+    // cout<<"\n";
+    ll nums[2] = {0, 0}, p = 1;
+    rep(i, 31) {
+        rep(j, 2) {
+            nums[j] += (p*a[j][i]);
+        }
+        p *= 2;
+    }
+    ll ans = nums[0]*nums[1];
+    cout<<ans<<"\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.precision(10);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ll tests = 1;
+    cin >> tests;
+    while(tests--) {
+        runcase();
+    }
+    return 0;
+}
