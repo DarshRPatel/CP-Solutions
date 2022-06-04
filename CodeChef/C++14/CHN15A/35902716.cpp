6bbabb8bf59e54a1f510abc462ad37dc43ef11de
Darsh Patel <47074897+DarshRPatel@users.noreply.github.com>
2022-06-05 01:17:00 +0530

    Committing files

---
+++ CodeChef/C++14/CHN15A/35902716.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<long long> vl;
+typedef pair<long long, long long> pl;
+
+#define F 						first
+#define S 						second
+#define PB 						push_back
+#define MP 						make_pair
+#define all(c) 					c.begin(), c.end()
+#define f(i,a,b) 				for(ll i=a; i<b; i++)
+#define rep(i,n) 				f(i,0,n)
+#define tr(c,i) 				for(typeof(c).begin() i = c.begin(); i != c.end(); i++)
+#define present(c,x) 			(c.find(x) != c.end())	//for set and map
+#define cpresent(c,x) 			(find(all(c),x) != c.end())	//for vectors
+#define w(t)					ll t; cin>>t; while(t--)
+#define epsilon 				1e-9
+#define MAX						100
+
+constexpr ll int_pow(ll b, ll e) {
+    return (e == 0) ? 1 : b * int_pow(b, e - 1);
+}
+
+void doublade() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.precision(10);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main()
+{
+	doublade();
+	w(t) {
+		ll n, k, temp, ans=0;
+		cin >> n >> k;
+		rep(i, n) {
+			cin>>temp;
+			ans += ((temp + k)%7==0) ? 1 : 0;
+		}
+		cout<<ans<<"\n";
+	}
+	return 0;
+}
