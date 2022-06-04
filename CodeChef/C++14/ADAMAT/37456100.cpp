940ac8c5f8007e88bb60ea25eaf11b0a76457362
Darsh Patel <47074897+DarshRPatel@users.noreply.github.com>
2022-06-05 01:15:50 +0530

    Committing files

---
+++ CodeChef/C++14/ADAMAT/37456100.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<long long> vl;
+typedef pair<long long, long long> pl;
+typedef vector<vector<long long>> matrix;
+
+#define F 						first
+#define S 						second
+#define PB 						push_back
+#define MP 						make_pair
+#define all(c) 					c.begin(), c.end()
+#define f(i,a,b) 				for(ll i=a; i<b; i++)
+#define rep(i,n) 				f(i,0,n)
+#define tr(c,i) 				for(typeof(c).begin() i = c.begin(); i != c.end(); i++)
+#define present(c,x) 			(c.find(x) != c.end())	//for set and map
+#define cpresent(c,x) 			(find(all(c),x) != c.end())	//for vectors
+#define w(t)					ll t; cin>>t; while(t--)
+#define epsilon 				1e-9
+#define MOD						(ll)1e9
+
+void doublade() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.precision(10);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main() {
+	doublade();
+	w(t) {
+		ll n;
+		cin >> n;
+		matrix m(n+1, vl(n+1));
+		f(i, 1, n+1) {
+			f(j, 1, n+1) {
+				cin>>m[i][j];
+			}
+		}
+		ll cnt = 0;
+		bool flag = true;
+		for(ll i=n; i>=2; i--) {
+			ll tmp = (i-1)*(n+1);
+			if(flag && m[i][i-1]!=tmp) {
+				cnt++;
+				flag = false;
+			}else if(!flag && m[i][i-1]==tmp) {
+				cnt++;
+				flag = true;
+			}
+		}
+		cout<<cnt<<"\n";
+	}
+	return 0;
+}
