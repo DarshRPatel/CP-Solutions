c4323514d70770caca4df2678fcfa0cefe796d1a
Darsh Patel <47074897+DarshRPatel@users.noreply.github.com>
2022-06-05 01:10:18 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_b/19024568.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<long long> vl;
+typedef pair<long long, long long> pl;
+typedef vector<vector<long long>> matrix;
+
+#define F 						first
+#define S 						second
+#define PB 						push_back
+#define MP 						make_pair
+#define all(c) 					c.begin(), c.end()
+#define f(i,a,b) 				for(ll i=a; i<b; i++)
+#define rep(i,n) 				f(i,0,n)
+#define fd(i, b, a)				for(ll i=b; i>=a; i--)
+#define repr(i,n)				fd(i, n-1, 0)
+#define tr(c,i) 				for(typeof(c).begin() i = c.begin(); i != c.end(); i++)
+#define present(c,x) 			(c.find(x) != c.end())	//for set and map
+#define cpresent(c,x) 			(find(all(c),x) != c.end())	//for vectors
+#define ps(num, places)			fixed<<setprecision(places)<<num //use as cout<<ps(x, y)<<"\n";
+#define sz(x) 					(ll)(x).size()
+
+const ld epsilon = 1e-9;
+const ll MOD = 1e9+7;
+
+bool comp(ll a, ll b) {
+	return (a > b);
+}
+
+ll binpow(ll a, ll b) {
+	ll res = 1;
+	while(b>0) {
+		if(b&1) res *= a;
+		a = a*a;
+		b >>= 1;
+	}
+	return res;
+}
+
+void runcase() {
+	ll n, k;
+	cin >> n >> k;
+	vl a(n);
+	rep(i, n) cin >> a[i];
+	vl cost(n);
+	cost[0] = 0;
+	cost[1] = abs(a[1]-a[0]);
+	f(i, 2, n) {
+		cost[i] = 1e9;
+		for(ll j=max(0LL, i-k); j<i; j++) cost[i] = min(cost[i], cost[j]+abs(a[i]-a[j]));
+	}
+	cout<<cost[n-1]<<"\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.precision(10);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	ll tests = 1;
+	// cin >> tests;
+	while(tests--) {
+		runcase();
+	}
+	return 0;
+}
