212837f4dbd667f257cbd1387574a4ae0c6ddae0
Darsh Patel <47074897+DarshRPatel@users.noreply.github.com>
2022-06-05 01:15:30 +0530

    Committing files

---
+++ CodeChef/C++14/PTUPLES/41847183.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<long long> vl;
+typedef pair<long long, long long> pl;
+typedef vector<vector<long long>> matrix;
+
+#define F                         first
+#define S                         second
+#define PB                         push_back
+#define MP                         make_pair
+#define all(c)                     c.begin(), c.end()
+#define f(i,a,b)                 for(ll i=a; i<b; i++)
+#define rep(i,n)                 f(i,0,n)
+#define fd(i, b, a)                for(ll i=b; i>=a; i--)
+#define repr(i,n)                fd(i, n-1, 0)
+#define tr(c,i)                 for(typeof(c).begin() i = c.begin(); i != c.end(); i++)
+#define present(c,x)             (c.find(x) != c.end())    //for set and map
+#define cpresent(c,x)             (find(all(c),x) != c.end())    //for vectors
+#define ps(num, places)            fixed<<setprecision(places)<<num //use as cout<<ps(x, y)<<"\n";
+#define sz(x)                     (ll)(x).size()
+
+const ld epsilon = 1e-9;
+const ll MOD = 1e9+7;
+
+const ll N = 1e6+5;
+vl isprime(N, true), prime, SPF(N), res(N, 0); 
+
+bool comp(ll a, ll b) {
+    return (a > b);
+}
+
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while(b>0) {
+        if(b&1) res *= a;
+        a = a*a;
+        b >>= 1;
+    }
+    return res;
+}
+
+void manipulated_seive() { 
+    isprime[0] = isprime[1] = false ;
+    for (long long int i=2; i<N ; i++) { 
+        if (isprime[i]) {  
+            prime.push_back(i);
+            SPF[i] = i; 
+        }  
+        for (long long int j=0; 
+             j < (int)prime.size() && 
+             i*prime[j] < N && prime[j] <= SPF[i]; 
+             j++) 
+        { 
+            isprime[i*prime[j]]=false; 
+            SPF[i*prime[j]] = prime[j];
+        } 
+    }
+    ll cnt = 0;
+    for(ll i=3; i+2<=N; i++) {
+        if(isprime[i] && isprime[i+2]) cnt++;
+        res[i+2] = cnt;
+    }
+} 
+
+void runcase() {
+    ll n;
+    cin >> n;
+    cout<<res[n]<<"\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.precision(10);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ll tests = 1;
+    cin >> tests;
+    manipulated_seive();
+    while(tests--) {
+        runcase();
+    }
+    return 0;
+}
