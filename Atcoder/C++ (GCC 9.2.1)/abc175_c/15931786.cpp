6c043c3c03a51285adc67db2a7d935e4b1c40991
Darsh Patel <47074897+DarshRPatel@users.noreply.github.com>
2022-06-05 01:11:15 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc175_c/15931786.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<long long> vl;
+typedef pair<long long, long long> pl;
+typedef vector<vector<long long>> matrix;
+
+#define F 						first
+#define S 						second
+#define PB 						push_back
+#define MP 						make_pair
+#define all(c) 					c.begin(), c.end()
+#define f(i,a,b) 				for(ll i=a; i<b; i++)
+#define rep(i,n) 				f(i,0,n)
+#define tr(c,i) 				for(typeof(c).begin() i = c.begin(); i != c.end(); i++)
+#define present(c,x) 			(c.find(x) != c.end())	//for set and map
+#define cpresent(c,x) 			(find(all(c),x) != c.end())	//for vectors
+#define w(t)					ll t; cin>>t; while(t--)
+#define epsilon 				1e-9
+#define MOD						(ll)1e9
+
+void doublade() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.precision(10);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main() {
+	doublade();
+	// w(t) {
+		ll x, k, d;
+		cin >> x >> k >> d;
+		if(x<0) x = abs(x);
+		ll p = x/d;
+		if(k<p) {
+			ll ans = x - (k*d);
+			cout<<ans<<"\n";
+		}else {
+			k -= p;
+			ll ans = x - (p*d);
+			if(k%2==0) cout<<ans<<"\n";
+			else cout<<abs(ans-d)<<"\n";
+		}		
+	// }
+	return 0;
+}
