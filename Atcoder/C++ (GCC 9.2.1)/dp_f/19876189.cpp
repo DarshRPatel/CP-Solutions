690e8f151a2702c153a5a32ed19082be487fdbfb
Darsh Patel <47074897+DarshRPatel@users.noreply.github.com>
2022-06-05 01:10:04 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_f/19876189.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<long long> vl;
+typedef pair<long long, long long> pl;
+typedef vector<vector<long long>> matrix;
+
+#define F                         first
+#define S                         second
+#define PB                         push_back
+#define MP                         make_pair
+#define all(c)                     c.begin(), c.end()
+#define f(i,a,b)                 for(ll i=a; i<b; i++)
+#define rep(i,n)                 f(i,0,n)
+#define fd(i, b, a)                for(ll i=b; i>=a; i--)
+#define repr(i,n)                fd(i, n-1, 0)
+#define tr(c,i)                 for(typeof(c).begin() i = c.begin(); i != c.end(); i++)
+#define present(c,x)             (c.find(x) != c.end())    //for set and map
+#define cpresent(c,x)             (find(all(c),x) != c.end())    //for vectors
+#define ps(num, places)            fixed<<setprecision(places)<<num //use as cout<<ps(x, y)<<"\n";
+#define sz(x)                     (ll)(x).size()
+
+const ld epsilon = 1e-9;
+const ll MOD = 1e9+7;
+
+bool comp(ll a, ll b) {
+    return (a > b);
+}
+
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while(b>0) {
+        if(b&1) res *= a;
+        a = a*a;
+        b >>= 1;
+    }
+    return res;
+}
+
+void runcase() {
+    string s, t;
+    cin >> s >> t;
+    ll n = s.length(), m = t.length();
+    matrix dp(n+1, vl(m+1, 0));
+    f(i, 1, n+1) {
+        f(j, 1, m+1) {
+            if(s[i-1]==t[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    string ans = "";
+    ll i=n, j=m;
+    while(i>0 && j>0) {
+        if(s[i-1]==t[j-1]) {
+            ans += s[i-1];
+            i--, j--;
+        }else {
+            if(dp[i-1][j]>=dp[i][j-1]) i--;
+            else j--;
+        }
+    }
+    reverse(all(ans));
+    cout<<ans<<"\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.precision(10);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ll tests = 1;
+    // cin >> tests;
+    while(tests--) {
+        runcase();
+    }
+    return 0;
+}
